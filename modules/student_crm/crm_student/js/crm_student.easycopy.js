034d3ff606be5967963d29fcdaae91390ed16c09
Kevin Miller <kemiller@csumb.edu>
2011-11-02 10:05:55 -0700

    Added easy-copy method so that fields like student IDs can be easily copied.

---
+++ modules/student_crm/crm_student/js/crm_student.easycopy.js
@@ -0,0 +1,18 @@
+(function($) {
+  
+  Drupal.behaviors.StudentCRMEasyCopy = {
+    
+    attach : function() {
+      $('.easy-copy').click(function() {
+        var size = $(this).innerWidth();
+        var content = $(this).html();
+        var $input = $('<input>').attr('value', content)
+                               .addClass('easy-copy')
+                               .css('width', (size + 15) + 'px');
+        $(this).replaceWith($input);
+        $input.select();
+      });
+    }
+  };
+
+})(jQuery);
