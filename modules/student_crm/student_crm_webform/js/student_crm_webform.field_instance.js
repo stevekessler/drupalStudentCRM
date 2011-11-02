41c8bd9e0b799b2a995bab98ad9366f5667f414c
Kevin Miller <kemiller@csumb.edu>
2011-11-01 23:16:54 -0700

    Bulked up options for the form refernce field.

---
+++ modules/student_crm/student_crm_webform/js/student_crm_webform.field_instance.js
@@ -0,0 +1,17 @@
+(function($) {
+
+  Drupal.behaviors.studentCRMWebformFieldSettings = {
+    
+    attach : function() {
+      $('#edit-instance-settings-email-address').change(function() {
+        if($(this).val() == 'field') {
+          $('#email-field-source-select').show();
+        }
+        else {
+          $('#email-field-source-select').hide();
+        }
+      })
+      .trigger('change');
+    }
+  };
+})(jQuery);
