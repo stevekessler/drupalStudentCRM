5563c1d5d7e49806eea414c94dcd953f1b706045
Kevin Miller <kemiller@csumb.edu>
2011-11-14 12:47:59 -0800

    Fixed accordion button addition by triggering only on accordioncrate callback.

--- modules/student_crm/crm_case/js/crm_case.form.js
+++ modules/student_crm/crm_case/js/crm_case.form.js
@@ -1,17 +1,18 @@
 (function($) {
 	Drupal.behaviors.CRMCaseForm = {
     attach: function() {
-      $('#edit-preview').addClass('element-invisible');
-      $('.ui-accordion-content:not(.ui-accordion-content:last)').each(function() {
-      	$(this).append('<input type="button" value="Continue" class="continue" />');
-      });
-      $('.ui-accordion-content .continue').click(function() {
-      	$(this).parent('.ui-accordion-content').next('h3').trigger('click');
-      	var position = $(this).parent('.ui-accordion-content').position();
-      	window.scrollTo(0, position.top);
-      });
-      $('.ui-accordion-header').each(function(index, element) {
-      	$(this).prepend('<span class="number">' + (index + 1) +'.</span>');
+      $('.field-group-accordion').bind( "accordioncreate", function(event, ui) {
+        $('.ui-accordion-content:not(.ui-accordion-content:last)').each(function() {
+        	$(this).append('<input type="button" value="Continue" class="continue" />');
+        });
+        $('.ui-accordion-content .continue').click(function() {
+        	$(this).parent('.ui-accordion-content').next('h3').trigger('click');
+        	var position = $(this).parent('.ui-accordion-content').position();
+        	window.scrollTo(0, position.top);
+        });
+        $('.ui-accordion-header').each(function(index, element) {
+        	$(this).prepend('<span class="number">' + (index + 1) +'.</span>');
+        });
       });
     }
   };
