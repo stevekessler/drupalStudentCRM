3628045f106c9bea43b2df06244078ef85f3eedf
Kevin Miller <kemiller@csumb.edu>
2011-12-19 13:35:34 -0800

    Cleaned up some code and removed the deprecated student_crm_blocks module.

--- modules/student_crm/student_crm_blocks/theme/ajax_block.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="ajax-loader" data-selector="#block-student-crm-blocks-<?php print $module; ?><?php print $delta; ?>"  aaa:live="polite">
-  <div class="loading"><abbr title="Loading data"></abbr></div>
-</div>
