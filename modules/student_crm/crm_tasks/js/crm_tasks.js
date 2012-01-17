fa5634f44379e6cb139388a08623de62afb97754
Kevin Miller <kemiller@csumb.edu>
2012-01-17 13:22:27 -0800

    Fixed problem where the task form would appar between JS loads.

--- modules/student_crm/crm_tasks/js/crm_tasks.js
+++ modules/student_crm/crm_tasks/js/crm_tasks.js
@@ -1,7 +1,6 @@
 (function($) {
   Drupal.behaviors.CRMTasks = {
     attach: function() {
-      $('#edit-task-modal').addClass('element-invisible');
       $('#show-add-tasks').fancybox({
         'onClosed' : function() {
           $('#edit-task-modal').addClass('element-invisible')
