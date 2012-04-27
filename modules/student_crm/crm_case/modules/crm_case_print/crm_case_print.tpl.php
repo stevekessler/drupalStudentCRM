af07c73c24009be87c1249fa20ae34a2010225c2
Kevin Miller <kemiller@csumb.edu>
2012-04-27 14:52:29 -0700

    Added CRM Print functionality. You can now print cases. Woot!

---
+++ modules/student_crm/crm_case/modules/crm_case_print/crm_case_print.tpl.php
@@ -0,0 +1,10 @@
+<div id="print-contact">
+	<h2>
+		<?php print $contact_title; ?>
+	</h2>
+	<?php print $contact; ?>
+</div>
+<div id="print-case">
+	<?php print $case; ?>
+</div>
+<div class="clear"></div>
