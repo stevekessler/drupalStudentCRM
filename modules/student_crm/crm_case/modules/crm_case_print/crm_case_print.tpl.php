a3ea042f3f44a9f667945a44a62e2857eef75cc4
Kevin Miller <kemiller@csumb.edu>
2012-04-30 16:08:31 -0700

    Added form submissions to be printed.

--- modules/student_crm/crm_case/modules/crm_case_print/crm_case_print.tpl.php
+++ modules/student_crm/crm_case/modules/crm_case_print/crm_case_print.tpl.php
@@ -8,3 +8,17 @@
 	<?php print $case; ?>
 </div>
 <div class="clear"></div>
+<?php if(count($forms)): ?>
+	<?php foreach($forms as $form): ?>
+		<h2><?php print $form['form']; ?></h2>
+		<?php foreach($form['submissions'] as $k => $submission): ?>
+			<h3>
+				<?php print t('Submission #%number', array('%number' => $k + 1)); ?>
+			</h3>
+			<div>
+				<?php print $submission; ?>
+			</div>
+		<?php endforeach; ?>
+	<?php endforeach; ?>
+
+<?php endif; ?>
