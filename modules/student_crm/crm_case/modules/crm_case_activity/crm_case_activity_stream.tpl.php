4a1014fa883481c7a3e6680476a824351d5c8034
Kevin Miller <kemiller@csumb.edu>
2012-06-06 13:32:26 -0700

    Made coder happy.

--- modules/student_crm/crm_case/modules/crm_case_activity/crm_case_activity_stream.tpl.php
+++ modules/student_crm/crm_case/modules/crm_case_activity/crm_case_activity_stream.tpl.php
@@ -1,3 +1,3 @@
 <div id="case-activity-stream">
-	woo
+  woo
 </div>
--- modules/student_crm/crm_case/modules/crm_case_print/crm_case_print.tpl.php
+++ modules/student_crm/crm_case/modules/crm_case_print/crm_case_print.tpl.php
@@ -1,24 +1,24 @@
 <div id="print-contact">
-	<h2>
-		<?php print $contact_title; ?>
-	</h2>
-	<?php print $contact; ?>
+  <h2>
+    <?php print $contact_title; ?>
+  </h2>
+  <?php print $contact; ?>
 </div>
 <div id="print-case">
-	<?php print $case; ?>
+  <?php print $case; ?>
 </div>
 <div class="clear"></div>
-<?php if(count($forms)): ?>
-	<?php foreach($forms as $form): ?>
-		<h2><?php print $form['form']; ?></h2>
-		<?php foreach($form['submissions'] as $k => $submission): ?>
-			<h3>
-				<?php print t('Submission #%number', array('%number' => $k + 1)); ?>
-			</h3>
-			<div>
-				<?php print $submission; ?>
-			</div>
-		<?php endforeach; ?>
-	<?php endforeach; ?>
+<?php if (count($forms)): ?>
+  <?php foreach ($forms as $form): ?>
+    <h2><?php print $form['form']; ?></h2>
+    <?php foreach ($form['submissions'] as $k => $submission): ?>
+      <h3>
+        <?php print t('Submission #%number', array('%number' => $k + 1)); ?>
+      </h3>
+      <div>
+        <?php print $submission; ?>
+      </div>
+    <?php endforeach; ?>
+  <?php endforeach; ?>
 
 <?php endif; ?>
